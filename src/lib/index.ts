a6054136a7debb2c6bf243a9638a9ccf92f9966c
Mauritz <mauritz.sjodin3@gmail.com>
2024-06-01 20:20:08 +0200

    feat: create datatypes and render game grid

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,27 @@
-// place files you want to import through the `$lib` alias in this folder.
+type PieceType = "normal";
+type Pos = { x: number, y: number };
+type Piece = { pieceType: PieceType; cells: Pos[]; };
+type GameState = { grid: (PieceType | null)[][], piece: Piece };
+
+export function init(): GameState {
+  const rows = 16;
+  const cols = 16;
+  const grid = Array<Array<PieceType | null>>(rows);
+  for (let i = 0; i < rows; i++) {
+    grid[i] = Array<null>(cols).fill(null);
+  }
+  const piece = newPiece();
+  piece.cells.forEach(pos => {
+    grid[pos.y][pos.x] = piece.pieceType;
+  });
+
+  return { grid: grid, piece: piece };
+}
+
+function randomPieceType(): PieceType {
+  return "normal";
+}
+
+function newPiece(): Piece {
+  return { pieceType: randomPieceType(), cells: [] }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { init } from "$lib/index.ts"
+
+  let game = init();
+</script>
+
+<h1>PieceType: { game.piece.pieceType }</h1>
+<div class="flex flex-col items-center">
+  {#each game.grid as row}
+    <div>
+    {#each row as _}
+      <div class="inline-block h-10 w-10 border border-black">&nbsp;</div>
+    {/each}
+    </div>
+  {/each}
+</div>
