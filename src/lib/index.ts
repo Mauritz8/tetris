e14b1e39b506eb8e50d9364a43b7ba180298d56f
Mauritz <mauritz.sjodin3@gmail.com>
2024-06-02 23:06:44 +0200

    feat: detect collision

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,13 @@
 import { Orientation, createPiece } from "$lib/piece";
-import type { Piece } from "$lib/piece";
+import type { Piece, Pos } from "$lib/piece";
 import { Shape } from "$lib/tetromino";
 
-type GameState = { grid: (Shape | null)[][], piece: Piece };
+type GameState = {
+  rows: number,
+  cols: number,
+  grid: (Shape | null)[][],
+  piece: Piece
+};
 
 export function init(): GameState {
   const rows = 16;
@@ -16,10 +21,22 @@ export function init(): GameState {
     grid[pos.y][pos.x] = piece.tetromino.shape;
   });
 
-  return { grid: grid, piece: piece };
+  return { rows: rows, cols: cols, grid: grid, piece: piece };
 }
 
 export function updatePiece(gameState: GameState, piece: Piece): void {
+  if (isOutsideHoriz(gameState, piece)) return;
+
+  if (isAtBottom(gameState, piece) || isCollision(gameState, piece)) {
+    const newPiece = createPiece({ x: 4, y: 3 }, Orientation.RIGHT, Shape.T) 
+    newPiece.cells.forEach(pos => {
+      gameState.grid[pos.y][pos.x] = newPiece.tetromino.shape;
+    });
+    gameState.piece = newPiece;
+    return;
+  }
+
+
   gameState.piece.cells.forEach(pos => {
     gameState.grid[pos.y][pos.x] = null;
   });
@@ -29,4 +46,19 @@ export function updatePiece(gameState: GameState, piece: Piece): void {
   });
 
   gameState.piece = piece;
+}
+
+const isOutsideHoriz = (gs: GameState, p: Piece) => 
+    p.cells.findIndex(pos => pos.x < 0 || pos.x >= gs.cols) !== -1
+
+const isAtBottom = (gs: GameState, p: Piece) =>
+    p.cells.findIndex(pos => pos.y >= gs.rows) !== -1
+
+const isCollision = (gs: GameState, p: Piece) => {
+  const collision = (cell: (Shape | null), pos: Pos) => {
+    return cell !== null && 
+        !gs.piece.cells.find(c => c.x === pos.x && c.y === pos.y)
+  }
+
+  return p.cells.findIndex(pos => collision(gs.grid[pos.y][pos.x], pos)) !== -1
 }
