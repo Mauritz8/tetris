9184577502fa540ea4354973173011a221c97272
Mauritz <mauritz.sjodin3@gmail.com>
2024-06-03 15:01:59 +0200

    feat: better styling. Start button, do not allow input before start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
   import { init, updatePiece } from "$lib/index.ts";
   import { rotate, moveRight, moveLeft, moveDown } from "$lib/piece.ts";
 
+  let started = false;
   let game = init();
 
   function onKeyDown(e) {
+    if (!started) return;
     let f;
     switch (e.key) {
       case "ArrowUp":
@@ -26,23 +28,33 @@
     game = game;
   }
 
-  setInterval(() => {
-    updatePiece(game, moveDown(game.piece));
-    game = game;
-  }, 1000);
+  function startGame() {
+    started = true;
+    setInterval(() => {
+      updatePiece(game, moveDown(game.piece));
+      game = game;
+    }, 500);
+  }
 </script>
 
-<div class="flex flex-col items-center h-screen justify-center">
-  {#each game.grid as row}
-    <div>
-    {#each row as cell}
-      <div class="inline-block h-10 w-10 border border-black
-          {cell === null ? '' : 'bg-green-900'}">
-        &nbsp;
+<div class="flex flex-col items-center h-screen justify-around bg-blue-800">
+  <h1 class="text-2xl font-bold text-white">Tetris</h1>
+  <div class="flex flex-col">
+    {#each game.grid as row}
+      <div>
+      {#each row as cell}
+        <div class="inline-block h-10 w-10 ring-1 ring-white
+            {cell === null ? 'bg-black' : 'bg-purple-500'}">
+          &nbsp;
+        </div>
+      {/each}
       </div>
     {/each}
-    </div>
-  {/each}
+  </div>
+  <button class="bg-cyan-500 rounded-full px-36 py-2 text-lg font-bold
+      text-white" on:click={startGame}>
+    Start
+  </button>
 </div>
 
 <svelte:window on:keydown={onKeyDown} />
